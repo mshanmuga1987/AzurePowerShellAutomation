4461545be26a5134cdd57e19dbe09074a6c8d701
jbernec <cchyne@gmail.com>
2017-08-02 16:55:20 -0500

    Update

--- LinuxCustomScriptExtension/New-AzureManagedVM.ps1
+++ LinuxCustomScriptExtension/New-AzureManagedVM.ps1
@@ -1,6 +1,6 @@
 ﻿<# 
 .SYNOPSIS
-   PowerShell function to create a new Managed Disk Azure VM
+   PowerShell function to create a new Managed Disk Azure VM.
 .DESCRIPTION
    PowerShell function to create a new Managed Disk Azure linux VM.
    It calls a Custom Script Extension type that invokes a bash script to install the LAMP Stack software
@@ -15,18 +15,17 @@
         PowerShell Language
 /#>
 Param(
-$SubscriptionName = "Trial Subscription",
-$Location = "southcentralus",
-$ResourceGroupName = "RGManagedDisks"
+    $SubscriptionName = "Trial",
+    $Location = "southcentralus",
+    $ResourceGroupName = "RGManagedDisks"
 )
 
 
 #Azure Logon
-try
-{
+try {
     $connectionName = "AzureRunAsConnection"
     # Get the connection "AzureRunAsConnection "
-    $servicePrincipalConnection=Get-AutomationConnection -Name $connectionName         
+    $servicePrincipalConnection = Get-AutomationConnection -Name $connectionName         
 
     "Logging in to Azure..."
     Add-AzureRmAccount `
@@ -36,40 +35,40 @@ try
         -CertificateThumbprint $servicePrincipalConnection.CertificateThumbprint 
 }
 catch {
-    if (!$servicePrincipalConnection)
-    {
+    if (!$servicePrincipalConnection) {
         $ErrorMessage = "Connection $connectionName not found."
         throw $ErrorMessage
-    } else{
+    }
+    else {
         Write-Error -Message $_.Exception
         throw $_.Exception
-            }
     }
+}
 Select-AzureRmSubscription -SubscriptionName $SubscriptionName
 
 #Remove the defined Resources if the Resource Group currently exists
 
-try{
-        "Removing existing Resources"
-        Find-AzureRmResource -ResourceGroupNameContains $ResourceGroupName | Remove-AzureRmResource -Force
-}catch {
-        $ErrorMessage = "Resource Group $ResourceGroupName not found."
-        Write-Host $ErrorMessage
-        }
+try {
+    "Removing existing Resources"
+    Find-AzureRmResource -ResourceGroupNameContains $ResourceGroupName | Remove-AzureRmResource -Force
+}
+catch {
+    $ErrorMessage = "Resource Group $ResourceGroupName not found."
+    Write-Host $ErrorMessage
+}
 
 #Recreate the Resource Group if not Available
 
-try
-{
+try {
     # Get the ResourceGroup "RGManagedDisks "
-    $ResourceGroup= Get-AzureRmResourceGroup -Name $ResourceGroupName -Location $Location -ErrorAction Stop         
+    $ResourceGroup = Get-AzureRmResourceGroup -Name $ResourceGroupName -Location $Location -ErrorAction Stop         
 }
 catch {
-        $ErrorMessage = "Resource Group $ResourceGroupName not found.Creating Target Resource Group"
-        Write-Host $ErrorMessage
-        New-AzureRmResourceGroup -Name $ResourceGroupName -Location $Location -Force
-    }
-    $ResourceGroup= Get-AzureRmResourceGroup -Name $ResourceGroupName -Location $Location
+    $ErrorMessage = "Resource Group $ResourceGroupName not found.Creating Target Resource Group"
+    Write-Host $ErrorMessage
+    New-AzureRmResourceGroup -Name $ResourceGroupName -Location $Location -Force
+}
+$ResourceGroup = Get-AzureRmResourceGroup -Name $ResourceGroupName -Location $Location
 
 
 #Create Managed Disk VM
@@ -77,14 +76,14 @@ catch {
 $VaultName = "vaultspr"
 $SecretName = "vmadminpassword"
 $myvmpassword = (Get-AzureKeyVaultSecret -VaultName $VaultName -Name $SecretName).SecretValue
-$credential = New-Object -TypeName System.Management.Automation.PSCredential("slocal",$myvmpassword)
+$credential = New-Object -TypeName System.Management.Automation.PSCredential("slocal", $myvmpassword)
 
 #Create VM properties
 $VMName = "centos68vm"
 $subnet = New-AzureRmVirtualNetworkSubnetConfig -Name subnet1 -AddressPrefix "192.168.1.0/27"
 $vNet = New-AzureRmVirtualNetwork -Name vnet -ResourceGroupName $ResourceGroupName -Location $Location -AddressPrefix "192.168.1.0/24" -Subnet $subnet
 $pip = New-AzureRmPublicIpAddress -Name pip -ResourceGroupName $ResourceGroupName -Location $Location -AllocationMethod Dynamic -IpAddressVersion IPv4 -DomainNameLabel $VMName
-$pip =Get-AzureRmPublicIpAddress -Name pip -ResourceGroupName $ResourceGroupName
+$pip = Get-AzureRmPublicIpAddress -Name pip -ResourceGroupName $ResourceGroupName
 $subnet = Get-AzureRmVirtualNetworkSubnetConfig -Name subnet1 -VirtualNetwork $vNet
 $nic = New-AzureRmNetworkInterface -Name nic -ResourceGroupName $ResourceGroupName -Location $Location -Subnet $subnet -PublicIpAddress $pip
 
@@ -95,11 +94,11 @@ $vm = New-AzureRmVMConfig -VMName $VMName -VMSize "standard_A5" | `
     -Skus "6.8" -Version latest | Add-AzureRmVMNetworkInterface -Id $nic.Id
 $vm = New-AzureRmVM -ResourceGroupName $ResourceGroupName -Location $Location -VM $vm
 
-#Deploy LAMP Stack via VMExtension
-$LampUri = "https://store0518.blob.core.windows.net/templates/lamp68-setup.sh?sv=2016-05-31&ss=b&srt=sco&sp=rwdlac&se=2017-12-21T00:51:20Z&st=2017-06-20T16:51:20Z&spr=https&sig=f%2FLnz4u40U0tGdmxBDfO6VgWdhHzj%2BRJMVK8UjMKTUI%3D"
+#Deploy LAP Stack via VMExtension
+$LampUri = "https://raw.githubusercontent.com/jbernec/AzurePowerShellAutomation/master/LinuxCustomScriptExtension/lamp68-setup.sh"
 $Settings = @{"fileUris"= @($LampUri);"commandToExecute"="sh lamp68-setup.sh"}
 Set-AzureRmVMExtension -Publisher Microsoft.Azure.Extensions -ResourceGroupName $ResourceGroupName -VMName $VMName `
--Name CustomScript -Settings $Settings -TypeHandlerVersion 2.0 -Location $Location -ExtensionType CustomScript
+    -Name CustomScript -Settings $Settings -TypeHandlerVersion 2.0 -Location $Location -ExtensionType CustomScript
 
 
 #Get-Variable | Remove-Variable -ErrorAction SilentlyContinue
