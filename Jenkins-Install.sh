359643da2b5b9f50c32eae61577ba47581404dad
jbernec <Charles.Chukwudozie.consultant@nielsen.com>
2018-01-30 10:43:00 -0600

    Update

--- Jenkins-Install.sh
+++ Jenkins-Install.sh
@@ -2,13 +2,14 @@
 # set up install of Jenkins
 
 # install Jenkins
+sudo su
 wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -
-sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
-sudo apt-get update
-sudo apt-get install jenkins
-sudo apt-get install php
-sudo apt-get update
+sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+apt-get update -y
+apt-get install -y jenkins
+apt-get install -y php
+apt-get update -y
 
 # sudo echo "7227-voya" | passwd --stdin root
-sudo echo -e "7227-voya\n7227-voya" | (passwd --stdin root)
+# sudo echo -e "7227-voya\n7227-voya" | (passwd --stdin root)
 # sudo echo -e "7227-voya\n7227-voya" | passwd root
