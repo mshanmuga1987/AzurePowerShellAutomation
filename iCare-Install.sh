525b808314b1e80683aeeee9ffef5691e42a53d4
jbernec <Charles.Chukwudozie.consultant@nielsen.com>
2018-01-31 09:39:10 -0600

    Update

--- iCare-Install.sh
+++ iCare-Install.sh
@@ -3,7 +3,7 @@
 
 # create dir
 sudo su
-mkdir -p /home/slocal/apps/icare
+mkdir -p /home/slocal/icareapp
 
 # sudo echo "7227-voya" | passwd --stdin root
 # sudo echo -e "7227-voya\n7227-voya" | (passwd --stdin root)
