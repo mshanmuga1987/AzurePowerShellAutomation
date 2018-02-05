efc1bbebdccd4628a0ac145ee7ccb794e83d0f4c
jbernec <Charles.Chukwudozie.consultant@nielsen.com>
2018-02-05 10:01:02 -0600

    Update

--- iCare-Install.sh
+++ iCare-Install.sh
@@ -4,6 +4,11 @@
 # create dir
 sudo su
 mkdir -p /home/slocal/icareapp
+apt-get install -y php libapache2-mod-php php-mcrypt php-mysql php-gd php-curl
+apt-get update -y
+apt-get install -y apache2
+apt-get install -y mysql-server
+apt-get install -y redis-server
 
 # sudo echo "7227-voya" | passwd --stdin root
 # sudo echo -e "7227-voya\n7227-voya" | (passwd --stdin root)
